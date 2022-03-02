03b34a499a5544d5a29448194a808f481b2d2f81
marinahaugen <marina.santos.haugen@dnb.no>
2022-03-02 16:04:46 +0100

    adds db

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,22 +1,66 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from "./Bloglist";
 
 const Home = () => {
-  const [name, setName] = useState("Per");
+  const [blogs, setBlogs] = useState(null);
+  const [name, setName] = useState("Anna");
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-  const handleClickLuigi = () => {
-    setName("Luigi");
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
   };
 
   const handleClickPer = () => {
     setName("Per");
   };
 
+  const handleClickOla = () => {
+    setName("Ola");
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if(!res.ok) {
+            throw Error('Could not fetch data for that resource')
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, []);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>Hei {name}!</p>
-      <button onClick={handleClickLuigi}>Change to Luigi</button>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title="All Blogs!"
+          handleDelete={handleDelete}
+        />
+      )}
+      {blogs && (
+        <BlogList
+          blogs={blogs.filter((blog) => blog.author === "Marina")}
+          title="Marinas Blogs!"
+        />
+      )}
       <button onClick={handleClickPer}>Change to Per</button>
+      <button onClick={handleClickOla}>Change to Ola</button>
+      <p>{name}</p>
     </div>
   );
 };
