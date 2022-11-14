b811d526974da50da54bd211fef351f5c8845e58
marinahaugen <marina.santos.haugen@dnb.no>
2022-11-14 19:58:48 +0100

    Solution 2 with T:O(n) and S:O(1)

--- Strings/removeDuplicateSortedArr.ts
+++ Strings/removeDuplicateSortedArr.ts
@@ -21,12 +21,55 @@ var removeDuplicates = function(nums) {
   let k = arrCount.indexOf('_');
   let numberOfEmpthySlots = nums.length - k;
   let empthySlots = new Array(numberOfEmpthySlots).fill('_');
-  console.log(empthySlots);
-  //TODO: convert count to number of occurances, first element should be nums[0]
-  let arr = arrCount.concat(empthySlots);
-  console.log(`Output: ${k}, ${arr}`);
+  let uniqueNumbers = new Array(k).fill(0);
+  let firstNumber = nums[0];
+  for(let i = 0; i < k; i++) {
+    if (firstNumber >= 0) {
+      uniqueNumbers[i] = i+firstNumber;
+    }
+    else {
+      //Not solved for negative cases - but k is the only value that is expected. So OK.
+    }
+  }
+  let arr = uniqueNumbers.concat(empthySlots);
+  console.log(`Output: ${k}, [${arr}]`);
 };
-//TODO: Test with negative numbers
+
 
 removeDuplicates([1,1,2]) //Output: 2, nums = [1,2,_]
 removeDuplicates([0,0,1,1,1,2,2,3,3,4]) //Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+removeDuplicates([-50,-40, -40, 20]) //Failing
+
+/*
+In-palce: not creating a new array
+Pass-by-reference
+Directly modify the paramter array
+Two-pointer scanning. Adjacent. Include number first time. Add to correct location.
+Ignore the first number. Smallest and unique.
+T = O(n) - since we only have 2 pointers, and both the pointers will traverse the array at most once.
+S = O(1) - since we are not using any extra space.
+*/
+function removeDuplicates2(nums) {
+
+  let n = nums.length;
+  if (n == 0) {
+    return 0;
+  }
+
+  let insertIndex = 1;
+  for(let i = 1; i < n; i++) {
+    //if different: it means that it is the first time we see the number
+    if(nums[i] != nums[i-1]) {
+      nums[insertIndex] = nums[i];
+      insertIndex++
+    }
+  }
+  console.log(insertIndex);
+  return insertIndex;
+}
+
+removeDuplicates2([1,1,2]) //Alt 2: 2, nums = [1,2,_]
+removeDuplicates2([0,0,1,1,1,2,2,3,3,4]) //Alt 2: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+removeDuplicates2([-50,-40, -40, 20]) //Alt 2: 3, nums = [-50,-40,20,_]
+removeDuplicates2([]) //0
+
